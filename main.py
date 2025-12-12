e3b7127209f846e8a1edea54ca00057c1582628c
NitRamCI <chavezimartin18@gmail.com>
2025-12-12 01:03:49 -0500

    terminado

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from time import sleep
 """
 PROYECTO 1 : CRUD DE EMPRESAS
-NOMBRE : MARTIN CHAVEZ IGLESIAS
+NOMBRE : MARTIN FREDDY CHAVEZ IGLESIAS
 """
 
 dic_empresas = {
@@ -33,26 +33,64 @@ while(True):
         print(" " * 10 + "REGISTRAR EMPRESA")
         print("=" * ANCHO)
         
+        ruc = input("INGRESE RUC : ")
+        razon_social = input("INGRESE RAZÓN SOCIAL : ")
+        direccion = input("INGRESE DIRECCIÓN : ")
+        dic_nueva_empresa = {
+            'razon_social' : razon_social,
+            'direccion' : direccion
+        }
+        dic_empresas[ruc] = dic_nueva_empresa
+        print("EMPRESA REGISTRADA CON ÉXITO")
+
     elif opcion == 2:
         print("=" * ANCHO)
         print(" " * 10 + "MOSTRAR EMPRESA")
         print("=" * ANCHO)
-        
+
+        for ruc,info in dic_empresas.items():
+            print(f"RUC : {ruc} ")
+            print(f"RAZÓN SOCIAL : {info['razon_social']}")
+            print(f"DIRECCIÓN : {info['direccion']}")
+            print("-" * ANCHO)
+            
     elif opcion == 3:
         print("=" * ANCHO)
         print(" " * 10 + "ACTUALIZAR  EMPRESA")
         print("=" * ANCHO)
-        
+        ruc = input("INGRESE RUC DE LA EMPRESA A ACTUALIZAR : ")
+        if ruc in dic_empresas:
+            print(f'RAZÓN SOCIAL ACTUAL : {dic_empresas[ruc]["razon_social"]}')
+            nueva_razon_social = input(f"INGRESE NUEVA RAZÓN SOCIAL ({dic_empresas[ruc]['razon_social']}) :")
+            nueva_direccion = input(f"INGRESE NUEVA DIRECCIÓN ({dic_empresas[ruc]['direccion']}) :")
+            if nueva_razon_social:
+                dic_empresas[ruc]['razon_social'] = nueva_razon_social
+            if nueva_direccion:
+                dic_empresas[ruc]['direccion'] = nueva_direccion
+            print("EMPRESA ACTUALIZADA CON ÉXITO")
+        else:
+            print("RUC NO ENCONTRADO")
+       
     elif opcion == 4:
         print("=" * ANCHO)
         print(" " * 10 + "ELIMINAR EMPRESA")
         print("=" * ANCHO)
         
+        ruc = input("INGRESE RUC DE LA EMPRESA A ELIMINAR : ")
+        if ruc in dic_empresas:
+            print(f"EMPRESA({dic_empresas[ruc]['razon_social']}) SE ELIMINARÁ")
+            print("-" * ANCHO)    
+            del dic_empresas[ruc]
+            print(f"EMPRESA ELIMINADA CON ÉXITO")    
+        else:
+            print("EMPRESA PARA EL RUC QUE INGRESO NO ENCONTRADO")    
     elif opcion == 5:
         print("=" * ANCHO)
         print(" " * 10 + "SALIENDO DEL PROGRAMA")
         print("=" * ANCHO)
         sleep(2)
         break
+    else:
+        print("OPCIÓN NO VÁLIDA")
     
     input("Presione ENTER para continuar...")
